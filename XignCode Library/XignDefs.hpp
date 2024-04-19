3ad883c4b036fe267baea60c66f3030f8437d209
Ambiente <roberts.justin180499@gmail.com>
2024-04-19 02:50:49 +0200

    -

--- XignCode Library/XignDefs.hpp
+++ XignCode Library/XignDefs.hpp
@@ -27,33 +27,7 @@ namespace XignCode
 	
 	enum _XclioFid
 	{
-		XclioFidSysEnterA = 0x0,
-		XclioFidSysEnterW = 0x1,
-		XclioFidSysExit = 0x2,
-		XclioFidInit = 0x3,
-		XclioFidCleanup = 0x4,
-		XclioFidProbe = 0x5,
-		XclioFidProbeEx = 0x6,
-		XclioFidCreateCodeBox = 0x7,
-		XclioFidCloseCodeBox = 0x8,
-		XclioFidProbeCodeBox = 0x9,
-		XclioFidProbeCodeBoxEx = 0xA,
-		XclioFidEncrypt = 0xB,
-		XclioFidDecrypt = 0xC,
-		XclioFidRsaCreate = 0xD,
-		XclioFidRsaClose = 0xE,
-		XclioFidRsaSetPublicKey = 0xF,
-		XclioFidRsaSetPrivateKey = 0x10,
-		XclioFidRsaPublicEncrypt = 0x11,
-		XclioFidRsaPublicDecrypt = 0x12,
-		XclioFidRsaPrivateEncrypt = 0x13,
-		XclioFidRsaPrivateDecrypt = 0x14,
-		XclioFidCheck = 0x15,
-		XclioFidRegisterCallback = 0x16,
-		XclioFidRsaGenerateKey = 0x17,
-		XclioFidRsaFreeBuffer = 0x18,
-		XclioFidSetup = 0x19,
-		XclioFidSendCommandVa = 0x1A,
+
 	};
 
 	typedef unsigned int (XIGNAPI* _XignCode_function_dispatch_t)(void** function_ptr, FUNCTION_DISPATCH_TYPE function_type);
