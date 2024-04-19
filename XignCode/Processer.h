29efc4ee07c26b805d02cbba52db4815cd486b36
Ambiente <44445929+theambiente@users.noreply.github.com>
2024-04-19 02:51:20 +0200

    Update Processer.h

--- XignCode/Processer.h
+++ XignCode/Processer.h
@@ -1,15 +1,1 @@
-#pragma once
-#include <Windows.h>
 
-void TitleWindow(LPCSTR WindowTitle) {
-	HWND WinTitle = FindWindowA(NULL, WindowTitle);
-	if (IsWindow(WinTitle))
-	{
-		ExitProcess(-1);
-	}
-}
-
-void Processer()
-{
-	TitleWindow("cheat engine settings");
-}
